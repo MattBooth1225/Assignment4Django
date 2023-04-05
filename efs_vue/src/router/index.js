33b7f10fc876bdb46cd4c1c6f85b9ea07cab1ea0
MattBooth1225 <111766064+MattBooth1225@users.noreply.github.com>
2023-04-05 11:48:10 -0500

    First Vue Push (Working Through Step 17)

---
+++ efs_vue/src/router/index.js
@@ -0,0 +1,62 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/components/Home.vue'
+import Auth from '@/components/Auth.vue'
+import Register from '@/components/Register.vue'
+import CustomerList from '@/components/CustomerList.vue'
+import CustomerCreate from '@/components/CustomerCreate.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/customer-list',
+    name: 'CustomerList',
+    component: CustomerList
+  },
+  {
+    path: '/customer-list/:msg',
+    name: 'CustomerUpdatedList',
+    component: CustomerList
+  },
+  {
+    path: '/customer-create',
+    name: 'CustomerCreate',
+    component: CustomerCreate
+  },
+  {
+    path: '/customer-create/:pk',
+    name: 'CustomerUpdate',
+    component: CustomerCreate
+  },
+  {
+    path: '/auth',
+    name: 'Auth',
+    component: Auth
+  },
+  {
+    path:'/register',
+    name: 'Register',
+    component: Register
+  } 
+]
+
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+
+export default router
+
